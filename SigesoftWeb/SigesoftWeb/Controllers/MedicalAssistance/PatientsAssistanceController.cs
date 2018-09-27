d93e50067ce7fa33542722d977772ee25d217a4e
amerchan33 <albertomerchan@hotmail.com>
2018-09-27 11:27:43 -0500

    otros

---
+++ SigesoftWeb/SigesoftWeb/Controllers/MedicalAssistance/PatientsAssistanceController.cs
@@ -0,0 +1,18 @@
+﻿using SigesoftWeb.Controllers.Security;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SigesoftWeb.Controllers.MedicalAssistance
+{
+    public class PatientsAssistanceController : Controller
+    {
+        [GeneralSecurity(Rol = "PatientsAssistance-BoardPatientsAssistance")]
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ SigesoftWeb/SigesoftWeb/Controllers/Warehouse/MovementProductsController.cs
@@ -0,0 +1,18 @@
+﻿using SigesoftWeb.Controllers.Security;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SigesoftWeb.Controllers.Warehouse
+{
+    public class MovementProductsController : Controller
+    {
+        [GeneralSecurity(Rol = "MovementProducts-BoardMovementProducts")]
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
