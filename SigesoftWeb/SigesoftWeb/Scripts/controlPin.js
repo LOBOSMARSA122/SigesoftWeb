91fbe4e18be29fc3622d739c848e5ef24d922e7d
jasongc <gutierrezjoseph40@gmail.com>
2018-10-16 15:27:39 -0500

    cambios

---
+++ SigesoftWeb/SigesoftWeb/Scripts/controlPin.js
@@ -0,0 +1,26 @@
+﻿$(document).ready(function () {
+    var $iconPin = $(".iconoPin");
+    var icon = `<i class="fa-thumb-tack"></i>`;
+  
+    $iconPin.on("click", function () {
+        $iconPin.addClass("iconPinRotate")
+        if ($iconPin.val() == "onPin") {
+            LoadTopDiagnostic();
+            MonthlyControls();
+        $(".colAside").removeClass("col-md-4");
+        $(".colMain").removeClass("col-md-8");
+        $(".colAside").prependTo(".contenedor-main");
+        $(this).val("offPin");
+        } else if ($iconPin.val() == "offPin") {
+            $iconPin.removeClass("iconPinRotate")
+            LoadTopDiagnostic();
+            MonthlyControls();
+            $(".colAside").addClass("col-md-4");
+            $(".colMain").addClass("col-md-8");
+            $(".colAside").appendTo(".contenedor-main");
+            $(this).val("onPin");
+        }
+    });
+
+    
+})
